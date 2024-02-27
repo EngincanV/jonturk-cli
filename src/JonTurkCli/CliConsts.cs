cdb13e99b0b957f4a656cdf825d9901e45a656a6
Salih <salihozkara00@gmail.com>
2024-02-27 18:33:56 +0300

    Create script for saved commands

--- src/JonTurkCli/CliConsts.cs
+++ src/JonTurkCli/CliConsts.cs
@@ -5,4 +5,6 @@ public static class CliConsts
     public static readonly string JonTurkRootFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".jonturk");
 
     public static readonly string JonTurkSaveFilePath = Path.Combine(JonTurkRootFolderPath, "commands.json");
+    
+    public static readonly string JonTurkScriptsFolderPath = Path.Combine(JonTurkRootFolderPath, "scripts");
 }
